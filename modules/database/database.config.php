5692aad4ec10770dd1902929d3c3478496d34b00
JessmannPengard <65504096+JessmannPengard@users.noreply.github.com>
2023-04-18 22:12:36 +0200

    Delete database.config.php

--- modules/database/database.config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Datos de conexión a la base de datos
-define("HOST_NAME", "localhost");
-define("DB_NAME", "sample");
-define("DB_USER", "root");
-define("DB_PASSWORD", "");
-
-// Prefijo para las tablas de la base de datos
-define("DATABASE_TABLES_PREFIX", "sample_");
