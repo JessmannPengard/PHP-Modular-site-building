e0db6def35e66d92564e286c31e546180652d30a
JessmannPengard <contacto@jessmann.com>
2023-04-28 15:40:47 +0200

    pull

---
+++ modules/phpmailer/src/mail.config.php
@@ -0,0 +1,7 @@
+<?php
+
+define("MAIL_HOST", "smtp-host");
+define("MAIL_USERNAME", "mail_username");
+define("MAIL_PASSWORD", "mail_password");
+define("MAIL_MYEMAIL", "your_email");
+define("MAIL_PORT", 465);
